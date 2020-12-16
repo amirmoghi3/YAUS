dc8d0c9f6b5af5215a17cf988293615344536e9c
امیرحسین مقیسه <siramirmoghi3@gmail.com>
2020-12-16 15:58:18 +0330

    change links

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM registry.vsf-co.ir/library/debian:buster-slim
-COPY  url-shortener .
-COPY .env.docker .env
-EXPOSE 8000
-ENTRYPOINT [ "./url-shortener"]
--- src/model/response.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Response {
-    pub message: String,
-    pub status: bool,
-    pub code: String,
-}
